048ffafdbc87d06b0bb2e4ddf074f2c21f0acefe
Alycia <akbutterworth@live.com>
2019-02-09 12:23:17 -0800

    create mirror.py, update translate docstring, update functions in readme

---
+++ R/mirror.R
@@ -0,0 +1,15 @@
+
+mirror <- function(image_path, direction = 'all') {
+
+  #' Mirror an image
+  #'
+  #'Mirror takes the path to an image and generates a mirrored version of that image in the horizontal direction, vertical direction, or both.
+  #'It returns an array of pixel values for the mirrored image(s).
+  #'
+  #' @param image_path string: the file path of the image to be mirrored
+  #' @param direction string: dirction of mirroring. Options: 'horizontal', 'vertical', 'all'. Default setting 'all'
+  #'
+  #' @return array: pixel values of the mirrored image(s)
+  #' @export
+
+}

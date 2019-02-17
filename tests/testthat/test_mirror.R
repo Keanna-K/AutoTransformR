92cb7e38e06ec8e32c0b89ec04d704cbcdcd7256
Alycia <akbutterworth@live.com>
2019-02-17 12:25:38 -0800

    Remove Python code, fix expect_equal statements

--- tests/testthat/test_mirror.R
+++ tests/testthat/test_mirror.R
@@ -17,29 +17,29 @@ test_that("Test that image file path is correct", {
 })
 
 test_that("Test for correct output dimensions", {
-  test_img = skimage.io.imread(test_image)
-  returned_arr_1a = pre.mirror(test_img, "horizontal")  # should return 1 image
-  returned_arr_1b = pre.mirror(test_img, "vertical")  # should return 1 image
-  returned_arr_2a = pre.mirror(test_img, "all")  # should return 2 images
-  returned_arr_2b = pre.mirror(test_img)  # all is default, should return 2 images
+  test_img <- readImage("test_img")
+  returned_arr_1a <- pre.mirror(test_img, "horizontal")  # should return 1 image
+  returned_arr_1b <- pre.mirror(test_img, "vertical")  # should return 1 image
+  returned_arr_2a <- pre.mirror(test_img, "all")  # should return 2 images
+  returned_arr_2b <- pre.mirror(test_img)  # all is default, should return 2 images
 
-  expect_is(dim(returned_arr_1a)[length(dim(returned_arr_1a))] == 1)
-  expect_is(dim(returned_arr_2a)[length(dim(returned_arr_2a))] == 2)
-  expect_is(dim(returned_arr_2b)[length(dim(returned_arr_2b))] == 2)
-  expect_is(dim(returned_arr_1a) == dim(test_img))
-  expect_is(dim(returned_arr_2a)[-length(dim(returned_arr_2b))] == dim(test_img)) # check that the 1st image in the array has the same dim as original
+  expect_equal(dim(returned_arr_1a)[length(dim(returned_arr_1a))], 1)
+  expect_equal(dim(returned_arr_2a)[length(dim(returned_arr_2a))], 2)
+  expect_equal(dim(returned_arr_2b)[length(dim(returned_arr_2b))], 2)
+  expect_equal(dim(returned_arr_1a), dim(test_img))
+  expect_equal(dim(returned_arr_2a)[-length(dim(returned_arr_2b))], dim(test_img)) # check that the 1st image in the array has the same dim as original
 })
 
 test_that("Test that horizontal and vertical mirroring are occuring when specified", {
   if(direction == "horizontal"){
-    expect_is(returned_arr_1a[,1,] == test_img[,ncol(test_img),])  # check horizontal mirroring occurs
+    expect_equal(returned_arr_1a[,1,], test_img[,ncol(test_img),])  # check horizontal mirroring occurs
   }
   if(direction == "vertical"){
-    expect_is(returned_arr_1a[1,,] == test_img[nrow(test_img),,])  # check vertical mirroring occurs
+    expect_equal(returned_arr_1a[1,,], test_img[nrow(test_img),,])  # check vertical mirroring occurs
   }
   if(direction == "all"){
-    expect_is(returned_arr_1a[,1,,1] == test_img[,ncol(test_img),])  # check horizontal mirroring on first image in array
-    expect_is(returned_arr_1a[1,,,2] == test_img[nrow(test_img),,])  # check vertical mirroring on second image in array
+    expect_equal(returned_arr_1a[,1,,1], test_img[,ncol(test_img),])  # check horizontal mirroring on first image in array
+    expect_equal(returned_arr_1a[1,,,2], test_img[nrow(test_img),,])  # check vertical mirroring on second image in array
   }
 
 })
